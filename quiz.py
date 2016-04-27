5cd09bd7148415e057ebee3010856c9555233abe
David Silveiro <david.silveiro95@gmail.com>
2016-04-27 19:59:11 +0100

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,132 @@
+
+
+
+def arithmetic_Ops():
+
+    arithmetic_Score = 0
+    arithmetic_Fail  = []
+
+    print("What does the '+' operator do?\n \
+           1) Add                        \n \
+           2) Subtract                   \n \
+           3) Modules                    \n")
+
+    ans_1 = int(raw_input("Answer: \ "))
+
+    if ans_1 == 1:
+        arithmetic_Score += 1
+        print("Correct!")
+
+    else:
+        print("Incorrect!")
+
+
+    print("What does the '-' operator do?\n \
+           1) Subtracts a variables falue\n \
+           2) Mutiples a variables falue \n \
+           3) All of the above           \n ")
+ 
+    ans_2 = int(raw_input("Answer: "))
+
+    if ans_2 == 1:
+        arithmetic_Score += 1 
+        print("Correct!")
+    else:
+        print("Incorrect!")
+         
+
+    print("What does the '*' operator do? \n \
+           1) Devides a variables value   \n \
+           2) Deletes a variables value   \n \
+           3) Mutiples a variables value  \n") 
+
+    ans_3 = raw_input("Answer: ")
+
+    if ans_3 == 3:
+        arithmetic_Score += 1
+        print("Correct!")
+    else:
+        print("Incorrect!")
+
+    print("What does the '/' operator do? \n \
+           1) Used for styling            \n \
+           2) Seperates statements        \n \
+           3) Used for devision           \n")
+
+    ans_4 = raw_input("Answer: ")
+
+    if ans_4 == 3:
+        arithmetic_Score += 1
+        print("Correct!")
+
+    else:
+        print("Incorrect!")
+
+
+    print("What does the '%' operator do? \n \
+          1)Modulus Operator and remainder\n \
+          of after an integer division    \n \
+          2)Mutiples the variable by 12   \n \
+          3)All of the above              \n")
+
+        if ans_1 == 1:
+        arithmetic_Score += 1
+        print("Correct!")
+
+    else:
+        print("Incorrect!")
+
+
+
+    print("What does the '++' operator do?\n
+           1)")
+
+    if ans_1 == 1:
+        arithmetic_Score += 1
+        print("Correct!")
+
+    else:
+        print("Incorrect!")
+
+
+    print("What does the '--' operator do?")
+
+    if ans_1 == 1:
+        arithmetic_Score += 1
+        print("Correct!")
+
+    else:
+        print("Incorrect!")
+
+
+
+
+    return ("Score: %s")% str(arthmetic_Score))
+
+
+
+
+
+
+
+def relational_Ops():
+
+def logical_Ops():
+
+def bitwise_Ops():
+
+def assignment_Ops():
+
+def memory_Functions():
+
+    #Memset
+    #Memmov
+    #memchr
+    #memcpy
+
+
+
+def main():
+
+    print("Author: David Silveiro                    \n \
+          
