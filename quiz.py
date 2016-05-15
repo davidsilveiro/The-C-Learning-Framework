be73c01caf93d65e51eef62e8df6e4325e0e1e76
David Silveiro <davidsilveiro@users.noreply.github.com>
2016-05-15 11:18:55 +0100

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -10,10 +10,10 @@ def memory_Functions():
 
 def main():
 
-    print("Author: David Silveiro                    \n \
+    print("Author: David Silveiro                  \n \
     
-    print("Would you like to start from a specific place, \
-           or perhaps an exact module?")
+    print("Would you like to start from the start? \n \
+          Or perhaps an exact level?               \n")
            
     print("1) From the beginning     \n \
            2) Level 1: Operations    \n \
