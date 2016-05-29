d02e0958f6af57195d549392aa73bfba5d98f392
David Silveiro <david.silveiro95@gmail.com>
2016-05-29 08:44:43 +0100

    Update memory_functions.py

--- modules/memory_functions.py
+++ modules/memory_functions.py
@@ -1,1 +1,53 @@
 
+def memory_Management():
+
+    # Calloc
+    print("  \n \)
+    
+    ans_1 = raw_input("Answer: ")
+    
+    if:
+        arithmetic_Score += 1
+        print("Correct!  ")
+        
+    else:
+        print("Incorrect!")
+    
+    # Free
+    print(" \n \)
+    
+    ans_2 = raw_input("Answer: ")
+    
+    if:
+        arithmetic_Score += 1
+        print("Correct!  ")
+
+    else:
+        print("Incorrect!")
+    
+    # malloc
+    print("What does the '>' operator do?  \n \)
+    
+    ans_3 = raw_input("Answer: ")
+    
+    
+    if:
+        arithmetic_Score += 1
+        print("Correct!  ")
+    
+    else:
+        print("Incorrect!")
+    
+    # realloc
+    print("What does the '<' operator do?  \n \)
+    
+    ans_4 = raw_input("Answer: ")
+    
+    if:
+        arithmetic_Score += 1
+        print("Correct!  ")
+    
+    else:
+        print("Incorrect!")
+    
+    return()
